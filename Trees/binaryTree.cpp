a604bd7dcb8bc774ac6a3c41874e693581d224f2
prajwol <prajwolsubedizzz@gmail.com>
2023-02-02 07:58:30 +0545

    tree

--- Trees/binaryTree.cpp
+++ Trees/binaryTree.cpp
@@ -1,41 +1,85 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-class node{
-    public:
-        int data;
-        node* left;
-        node* right;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
 
-        node(int d){
-            this -> data = d;
-            this -> left = NULL;
-            this -> right = NULL;
-
-        }
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
 };
 
-node* buildTree(node* root){
+node *buildTree(node *root)
+{
     int data;
-    cout<<"Enter the data: "<<endl;
-    cin>>data;
+    cout << "Enter the data: " << endl;
+    cin >> data;
     root = new node(data);
 
-    if(data == -1){
+    if (data == -1)
+    {
         return NULL;
     }
 
-    cout<<"Enter data for the left side"<<endl;
+    cout << "Enter data for the left side" << endl;
     root->left = buildTree(root->left);
-    cout<<"Enter data for right side"<<endl;
+    cout << "Enter data for right side" << endl;
     root->right = buildTree(root->right);
     return root;
 }
-int main(){
-    node* root = NULL;
 
-    //creating a tree
+void levelOrderTraversal(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        { // earlier level is traversed completely
+            cout << endl;
+            if (!q.empty())
+            { // still elements in queue
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main()
+{
+    node *root = NULL;
+
+    // creating a tree
     root = buildTree(root);
-    return 0;
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
 
+    // level order
+    cout << "Level order Display: " << endl;
+    levelOrderTraversal(root);
+    return 0;
 }
